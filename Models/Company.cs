85f835c43029dacb79a0aac842b11b8a3632a37f
Gelan007 <anatoliy.fil033@gmail.com>
2021-12-01 21:01:00 +0200

    Create Company
    Создание полностью рабочего Company

---
+++ Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestRoadBack.Models
+{
+    public class Company
+    {
+        public int Company_id { get; }
+        public string Name { get; set; }
+        public string Company_check { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Company_account { get; set; }
+        public bool Is_comfirmed { get; set; }
+        public string Adress { get; set; }
+
+    }
+}
